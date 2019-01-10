b135e1b50307e243f725a5c4f594c3945c3c1444
Tendai Chikake <xulwrk@gmail.com>
2019-01-10 12:51:31 +0300

    node install

---
+++ public/js/index.js
@@ -0,0 +1,33 @@
+	$(function(){
+	
+	function internationale(){
+		/**
+		 * Controls the active language and memory management of user preferred language
+		 */
+		var lang = getCookie("lang");
+		if(lang==""){
+			setCookie("lang","ru",365);
+			lang = "ru";
+		}
+
+		$(".lang").addClass("hidden");
+		$(".lang").css("visibility","visible");
+		$(".lang."+lang).removeClass("hidden");
+	}	
+	function toggleInternationale(){
+		/** 
+		 *  reads the selected language in the triggering control, updates the user preferred language and calls the language control function `internationale`
+		 */
+		var lang = $(this).find(":selected").text();
+		setCookie("lang",lang,365);
+		internationale();
+	}
+	function main(){
+		//update copyrights sections
+		$(".copyright-date").html((new Date().getFullYear()));	
+		internationale();
+		$(".international").val(getCookie("lang")).on("change",toggleInternationale);
+		$("title,.title").html("Маркеты");
+	}
+	main();
+})
