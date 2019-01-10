03512d274e6c36089a2b761339479c565244d676
Tendai Chikake <xulwrk@gmail.com>
2019-01-10 14:49:43 +0300

    successful deploy

--- sync.sh
+++ sync.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -rRq --exclude sync.sh --exclude .env.example --exclude .git --exclude readme.md --exclude tests  --exclude .gitignore --exclude composer.lock ./ tendai@flequesboard.com:~/iflavarel.ilearnfb.com/
+rsync -rRql --exclude sync.sh --exclude .env.example --exclude .git --exclude readme.md --exclude tests  --exclude .gitignore --exclude composer.lock ./ tendai@flequesboard.com:~/iflavarel.ilearnfb.com/
